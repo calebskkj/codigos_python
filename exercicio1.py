ba5aeebbc3319d5c9b04cd32a4bc10d55df7b47a
calebskkj <calebehenrique009@gmail.com>
2024-09-03 17:17:51 -0300

    Atividade 1 e 2

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+anos = float(input("Escreva a sua idade, e a mesma será expressa em meses e dias:"))
+dias = anos *365
+meses = anos *12
+
+print(f"{anos} anos é igual a {meses} meses e {dias} dias")
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+nome_fzd = input("Digite o nome da sua fazenda: ")
+qtd_cvl = float(input("digite a quantidade de cavalos: "))
+qtd_frdr = qtd_cvl *4
+
+print(f"A fazenda {nome_fzd} possui {qtd_cvl} cavalos, a quantidade de ferraduras para equipar todos os cavalos é: {qtd_frdr}")
