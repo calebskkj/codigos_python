0823a0db2ac0b589d37d2c3eb15c1a32afb509fd
calebskkj <calebehenrique009@gmail.com>
2024-09-03 15:05:03 -0300

    trabalhando com comando de entrada e saída de python

---
+++ exemplo2.py
@@ -0,0 +1,13 @@
+nome= "calebskkj"
+end = "Santa Cruz"
+idade = 17 
+
+# print (nome, end, idade)
+# 1ª forma de concatenação
+print("Olá ",nome,"seu endereço é ",end,"sua idade é ", idade)
+
+# 2ª forma de concatenação
+print ("Seja bem vindo {}, sua residência está no {} e você possui {} anos.".format(nome, end, idade))
+
+# 3ª forma de concatenação
+print(f"Olá, seja bem vindo {nome}, o seu endereço é no {end} e a sua idade é {idade}")
