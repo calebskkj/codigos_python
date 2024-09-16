8bf9d5785281d862ac5cdd945b134135e332b67e
calebskkj <calebehenrique009@gmail.com>
2024-09-16 14:14:46 -0300

    trabalhando com estrutura enquanto no python

---
+++ estrutura_enquanto.py
@@ -0,0 +1,26 @@
+#1ª forma de utilizar while - semelhante ao FOR
+
+contador = 1
+
+while contador <= 5:
+    print(contador)
+    contador = contador + 1 #é o mesmo quecontador +=1
+
+#2ª forma de utilizar o enquanto - loop condicional normal
+valor = 0 
+
+while valor >=0:
+    valor = int (input("informe um valor qualquer digite um valor negativo para terminar: "))
+
+    print(f"você digitou {valor}")
+
+print("="*40)
+#3ª forma de utilizar o enquanto - semelhante a estrutura faça...enquanto
+while True:
+    senha = input("Informe sua senha: ")
+
+    if senha == "123":
+        print("Parabéns, senha correta")
+        break #forma de encerrar o loop
+    else:
+        print ("Senhas não conferem, tente novamente")
