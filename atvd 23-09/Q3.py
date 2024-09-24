7f4f7f92f123007a8c5ed6c02f843eed2deaf1e4
calebskkj <calebehenrique009@gmail.com>
2024-09-24 18:37:52 -0300

    atividades dia 24/09

--- atvd 23-09/Q3.py
+++ atvd 23-09/Q3.py
@@ -1,12 +1,12 @@
 import time
 def contador(segundos): 
     
-    for jj in range (segundos,0,-1):
-        print(jj)
+    for ctg in range (segundos,0,-1):
+        print(ctg)
         time.sleep(1)
 
 
-segundos = int(input("INFORME A QUNATIDADE DE SEGUNDOS: "))
+segundos = int(input("INFORME A QUANTIDADE DE SEGUNDOS: "))
 
 print(contador(segundos))
 
--- atvd 23-09/Q4.py
+++ atvd 23-09/Q4.py
@@ -1,26 +1,26 @@
 import random
-emb = random.randint(1,3)
+ale = random.randint(1,3)
 
 escolha = int(input("Escolha: \n 1-Tesoura \n 2-Pedra \n 3-Papel\n"))
 
-finaliz =1
+finaliz = 1
 while finaliz!=0:
-    if  emb ==1 and escolha ==3:
+    if  ale == 1 and escolha ==3:
         print("robo ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
-    elif emb==1 and escolha==2:
+    elif ale==1 and escolha==2:
         print("Você ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
-    elif emb==2 and escolha ==3:
+    elif ale==2 and escolha ==3:
         print("Você ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
-    elif emb == 3 and escolha ==1:
+    elif ale == 3 and escolha ==1:
         print("você ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
-    elif emb==2 and escolha ==1:
+    elif ale==2 and escolha ==1:
         print("robo ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
-    elif emb ==3 and escolha ==2:
+    elif ale ==3 and escolha ==2:
         print("robo ganhou")
         finaliz =int(input("voc quer continuar: sim: 1 não:0"))
     escolha = int(input("Escolha: \n 1-Tesoura \n 2-Pedra \n 3-Papel\n"))
